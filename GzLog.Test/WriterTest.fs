db894a2e939d658b0056409a7c671ea627d25b78
Radek Micek <radek.micek@gmail.com>
2023-01-29 20:52:03 +0100

    Log writer

---
+++ GzLog.Test/WriterTest.fs
@@ -0,0 +1,221 @@
+﻿module GzLog.Test.Writer
+
+open System
+open System.IO
+
+open NUnit.Framework
+
+open GzLog
+
+type TempDirectory() =
+    let mutable disposed = false
+    let info = Directory.CreateTempSubdirectory("GzLogTest")
+
+    member _.Path = info.FullName
+
+    interface IDisposable with
+        override _.Dispose() =
+            if not disposed then
+                try
+                    info.Delete(true)
+                // TODO Don't catch all exceptions - eg. `OutOfMemoryException`.
+                with e ->
+                    eprintfn "Delete during Dispose failed: %A" e
+
+let config = { MaxDecompressedMemberSize = 5
+               MaxFileSize = 1024
+               MaxSecondsBeforeFlush = 60
+               DirFunc = fun _ -> failwith "Missing DirFunc" }
+
+let dt = DateTimeOffset(2023, 1, 29, 9, 0, 0, TimeSpan.Zero)
+
+type MemberData = byte array
+type FileName = string
+type DirName = string
+
+let readMembers (file : string) : MemberData list =
+    use stream = new FileStream(file, FileMode.Open)
+    let n = 1024 * 1024
+    Utils.membersFromStream stream n n
+    |> Seq.map (fun m -> m.Decompressed.ToArray())
+    |> Seq.toList
+
+let checkLogs (baseDir : string) (expected : list<DirName * FileName * MemberData list>) =
+    let expected =
+        expected
+        |> List.groupBy (fun (dir, _, _) -> dir)
+        |> List.map (fun (dir, filesInDir) ->
+            filesInDir
+            |> List.map (fun (_, file, members) -> file, members)
+            |> Map.ofList
+            |> fun map -> dir, map)
+        |> Map.ofList
+
+    CollectionAssert.IsEmpty(Directory.GetFiles(baseDir), "Files in base dir")
+    // Order of dirs is not important.
+    CollectionAssert.AreEquivalent(
+        expected.Keys,
+        Directory.GetDirectories(baseDir) |> Array.map Path.GetFileName,
+        "Directories in base dir")
+
+    expected
+    |> Map.iter (fun dir filesInDir ->
+        let path = Path.Combine(baseDir, dir)
+
+        CollectionAssert.IsEmpty(Directory.GetDirectories(path), $"Directories in %s{dir}")
+        // Order of files is not important.
+        CollectionAssert.AreEquivalent(
+            filesInDir.Keys,
+            Directory.GetFiles(path) |> Array.map Path.GetFileName,
+            $"Files in %s{dir}")
+
+        filesInDir
+        |> Map.iter (fun file members ->
+            let path = Path.Combine(path, file)
+            // Order of members is important.
+            CollectionAssert.AreEqual(members, readMembers path, $"Members of %s{dir}/%s{file}")))
+
+[<Test>]
+let ``date in path is greater or equal than date in message timestamp`` () =
+    use baseDir = new TempDirectory()
+    let config = { config with DirFunc = fun date -> Path.Combine(baseDir.Path, date) }
+
+    use writer = new LogWriter(config)
+    // Must end in `2023-01-29`.
+    writer.AppendMessage(dt, "A"B)
+    // Must end in `2023-01-30`.
+    writer.AppendMessage(dt.AddDays 1, "B"B)
+    writer.AppendMessage(dt, "C"B)
+    writer.AppendMessage(dt.AddDays 1, "D"B)
+    writer.FlushMember()  // We need to flush before checking.
+
+    checkLogs baseDir.Path
+        [ "2023-01-29", "log_090000.gz", [ "A"B ]
+          "2023-01-30", "log_090000.gz", [ "BCD"B ] ]
+
+[<Test>]
+let ``capacity of member is not exceeded`` () =
+    use baseDir = new TempDirectory()
+    let config = { config with DirFunc = fun date -> Path.Combine(baseDir.Path, date) }
+
+    use writer = new LogWriter(config)
+    writer.AppendMessage(dt, "AB"B)
+    writer.AppendMessage(dt, "CD"B)
+    writer.AppendMessage(dt, "EF"B)  // Goes to new member.
+    writer.AppendMessage(dt, "GH"B)
+    writer.AppendMessage(dt, "I"B)
+    writer.AppendMessage(dt, "J"B)  // Goes to new member.
+    writer.FlushMember()  // We need to flush before checking.
+
+    checkLogs baseDir.Path
+        [ "2023-01-29", "log_090000.gz", [ "ABCD"B; "EFGHI"B; "J"B ] ]
+
+[<Test>]
+let ``capacity of member can be exceeded by single oversized message`` () =
+    use baseDir = new TempDirectory()
+    let config = { config with DirFunc = fun date -> Path.Combine(baseDir.Path, date) }
+
+    use writer = new LogWriter(config)
+    writer.AppendMessage(dt, "A"B)
+    writer.AppendMessage(dt, "BCDEFGHI"B)  // This message is oversized and must be alone in single member.
+    writer.AppendMessage(dt, "J"B)
+    writer.FlushMember()  // We need to flush before checking.
+
+    checkLogs baseDir.Path
+        [ "2023-01-29", "log_090000.gz", [ "A"B; "BCDEFGHI"B; "J"B ] ]
+
+[<Test>]
+let ``capacity of file is not exceeded`` () =
+    use baseDir = new TempDirectory()
+    let config = { config with DirFunc = fun date -> Path.Combine(baseDir.Path, date) }
+
+    // Create huge data which after compression fits 2 times into single file.
+    // FIXME: This test is fragile and depends on compression ratio which could change.
+    let random = Random(20)
+    let hugeData = Array.init 480 (fun _ -> random.Next() |> byte)
+
+    use writer = new LogWriter(config)
+    writer.AppendMessage(dt, hugeData)
+    writer.AppendMessage(dt.AddSeconds 1, hugeData)
+    writer.AppendMessage(dt.AddSeconds 1, hugeData)  // Goes to new file.
+    writer.FlushMember()  // We need to flush before checking.
+
+    checkLogs baseDir.Path
+        [ "2023-01-29", "log_090000.gz", [ hugeData; hugeData ]
+          "2023-01-29", "log_090001.gz", [ hugeData ] ]
+
+[<Test>]
+let ``capacity of file can be exceeded by single oversized message`` () =
+    use baseDir = new TempDirectory()
+    let config = { config with DirFunc = fun date -> Path.Combine(baseDir.Path, date) }
+
+    // Create huge data which after compression doesn't fit into file capacity.
+    // FIXME: This test is fragile and depends on compression ratio which could change.
+    let random = Random(20)
+    let hugeData = Array.init (int config.MaxFileSize) (fun _ -> random.Next() |> byte)
+
+    use writer = new LogWriter(config)
+    writer.AppendMessage(dt, "A"B)
+    writer.AppendMessage(dt.AddSeconds 1, hugeData)  // Goes to new file.
+    writer.AppendMessage(dt.AddSeconds 2, "Z"B)  // Goes to new file.
+    writer.FlushMember()  // We need to flush before checking.
+
+    checkLogs baseDir.Path
+        [ "2023-01-29", "log_090000.gz", [ "A"B ]
+          "2023-01-29", "log_090001.gz", [ hugeData ]
+          "2023-01-29", "log_090002.gz", [ "Z"B ] ]
+
+[<Test>]
+let ``files are ordered lexicographically`` () =
+    use baseDir = new TempDirectory()
+    let config = { config with DirFunc = fun date -> Path.Combine(baseDir.Path, date) }
+
+    // Create huge data which after compression doesn't fit into file capacity.
+    // FIXME: This test is fragile and depends on compression ratio which could change.
+    let random = Random(20)
+    let hugeData = Array.init (int config.MaxFileSize) (fun _ -> random.Next() |> byte)
+
+    use writer = new LogWriter(config)
+    writer.AppendMessage(dt, hugeData)
+    writer.AppendMessage(dt, hugeData)  // Goes to new file.
+    writer.AppendMessage(dt, hugeData)  // Goes to new file.
+    writer.FlushMember()  // We need to flush before checking.
+
+    checkLogs baseDir.Path
+        [ "2023-01-29", "log_090000.gz", [ hugeData ]
+          "2023-01-29", "log_090000x.gz", [ hugeData ]  // `x` must be appended because timestamp is same.
+          "2023-01-29", "log_090000xx.gz", [ hugeData ] ]
+
+[<Test>]
+let ``dispose flushes data`` () =
+    use baseDir = new TempDirectory()
+    let config = { config with DirFunc = fun date -> Path.Combine(baseDir.Path, date) }
+
+    let _ =
+        use writer = new LogWriter(config)
+        writer.AppendMessage(dt, "AB"B)
+
+        // Ensure that data were not flushed.
+        checkLogs baseDir.Path []
+
+        // Dispose is called here.
+
+    checkLogs baseDir.Path
+        [ "2023-01-29", "log_090000.gz", [ "AB"B ] ]
+
+[<Test>]
+let ``empty message is ignored`` () =
+    use baseDir = new TempDirectory()
+    let config = { config with DirFunc = fun date -> Path.Combine(baseDir.Path, date) }
+
+    use writer = new LogWriter(config)
+    writer.AppendMessage(dt, ""B)
+    writer.FlushMember()
+
+    checkLogs baseDir.Path []
+
+// TODO
+// - Message with big enough timestamp causes flush.
+// - Big enough timestamp causes flush.
+// - Not big enough timestamp doesn't cause flush.
+// - Dispose doesn't throw exception.
---
+++ GzLog/Writer.fs
@@ -0,0 +1,245 @@
+﻿namespace GzLog
+
+open System
+open System.Buffers
+open System.IO
+
+open LibDeflateGzip
+
+/// Has no path and no extension.
+type private FileWithoutExt = string
+
+type private FileState =
+    { DateInPath : string
+      Name : FileWithoutExt
+      Size : int64
+    }
+
+type private DecompressedMemberState =
+    { Timestamp : DateTimeOffset
+      Size : int
+    }
+
+type private LogWriterState =
+    | Initial
+    | EmptyMember of FileState
+    // Non-empty member has decompressed member size > 0.
+    // If message timestamps are monotone then all messages in member belong to date `DateInPath`.
+    // Otherwise all messages in member belong to date `DateInPath` or earlier date.
+    | NonEmptyMember of FileState * DecompressedMemberState
+
+type LogWriterConfig =
+    { MaxDecompressedMemberSize : int
+      MaxFileSize : int64  // This is compressed size.
+      MaxSecondsBeforeFlush : int
+      DirFunc : string -> string  // Pure function. Must be injective.
+    }
+
+[<Sealed>]
+type LogWriter(config : LogWriterConfig) =
+    let filePrefix = "log_"
+    let dataExtension = ".gz"
+    let sizeExtension = ".last-size"
+
+    let mutable disposed = false
+    // Set to false before appending to files.
+    // If appending fails due to an exception in the middle then `LogWriter`
+    // is in uncertain state. It doesn't know if some or all data were written
+    // to file and whether the file isn't corrupted.
+    // Fortunately if such situation happens `lastAppendFinished` remains set to false
+    // and `LogWriter` becomes unusable.
+    let mutable lastAppendFinished = true
+
+    let mutable state = Initial
+
+    let mutable decompressedMemberBuffer = MemoryPool.Shared.Rent(2 * 1024 * 1024)
+    let mutable compressedMemberBuffer = MemoryPool.Shared.Rent(2 * 1024 * 1024)  // Consider making size configurable.
+    let compressor = new Compressor(12)  // Consider making compression level configurable.
+
+    let check () =
+        if not lastAppendFinished then
+            failwith "Last append haven't finished"
+        if disposed then
+            raise (ObjectDisposedException(nameof LogWriter))
+
+    let timestampToDate (timestamp : DateTimeOffset) =
+        timestamp.ToString("yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture)
+
+    let timestampToFile (timestamp : DateTimeOffset) : FileWithoutExt =
+        filePrefix + timestamp.ToString("HHmmss", System.Globalization.CultureInfo.InvariantCulture)
+
+    // Returns file names with extension.
+    let listLogs dir =
+        try
+            Directory.GetFiles dir
+            |> Array.map Path.GetFileName
+            |> Array.filter (fun file ->
+                let extOk =
+                    file.EndsWith(dataExtension, StringComparison.InvariantCultureIgnoreCase) ||
+                    file.EndsWith(sizeExtension, StringComparison.InvariantCultureIgnoreCase)
+                extOk && file.StartsWith(filePrefix, StringComparison.InvariantCultureIgnoreCase))
+        with :? DirectoryNotFoundException -> [||]
+
+    let getLexicographicallyBiggerFile (timestamp : DateTimeOffset) (lastFile : FileWithoutExt) : FileWithoutExt =
+        let newFile = timestampToFile timestamp
+        if lastFile < newFile
+        then newFile
+        else lastFile + "x"  // Create different name which is lexicographically AFTER.
+
+    let createInitialFileState (timestamp : DateTimeOffset) : FileState =
+        let today = timestampToDate timestamp
+        let tomorrow = timestampToDate (timestamp.AddDays 1)
+
+        let dateInPath, existingLogs =
+            match listLogs (config.DirFunc tomorrow) with
+            | [||] -> today, listLogs (config.DirFunc today)
+            | logs -> tomorrow, logs
+
+        let file =
+            match existingLogs with
+            | [||] -> timestampToFile timestamp
+            | logs ->
+                logs
+                |> Seq.map Path.GetFileNameWithoutExtension
+                |> Seq.max
+                |> getLexicographicallyBiggerFile timestamp
+        { DateInPath = dateInPath
+          Name = file
+          Size = 0L
+        }
+
+    member _.FlushMember() =
+        check ()
+
+        match state with
+        | Initial | EmptyMember _ -> ()
+        | NonEmptyMember (fileState, decompressedMemberState) ->
+            // Compress member.
+            let mutable compressedMemberSize = 0
+            while compressedMemberSize = 0 do
+                compressedMemberSize <-
+                    compressor.Compress(
+                        Span.op_Implicit(decompressedMemberBuffer.Memory.Span.Slice(0, decompressedMemberState.Size)),
+                        compressedMemberBuffer.Memory.Span)
+                // `compressedMemberBuffer` wasn't big enough.
+                if compressedMemberSize = 0 then
+                    let newSize = 2 * compressedMemberBuffer.Memory.Length
+                    compressedMemberBuffer.Dispose()
+                    compressedMemberBuffer <- MemoryPool.Shared.Rent(newSize)
+
+            let fileState =
+                // If the file is empty then we use it.
+                // Otherwise we check whether member fits into file
+                // and if not then we use another file.
+                if fileState.Size > 0 && config.MaxFileSize - fileState.Size < compressedMemberSize then
+                    { DateInPath = fileState.DateInPath
+                      Name = getLexicographicallyBiggerFile decompressedMemberState.Timestamp fileState.Name
+                      Size = 0L
+                    }
+                else fileState
+
+            let dir = config.DirFunc fileState.DateInPath
+            let file = Path.Combine(dir, fileState.Name)
+            let sizeFile = file + sizeExtension
+            let dataFile = file + dataExtension
+
+            lastAppendFinished <- false
+            Directory.CreateDirectory(dir) |> ignore
+            File.WriteAllText(sizeFile, string fileState.Size)
+
+            // Append compressed member to file.
+            let _ =
+                use stream = new FileStream(dataFile, FileMode.Append)
+                stream.Write(compressedMemberBuffer.Memory.Span.Slice(0, compressedMemberSize))
+            // Do we need to flush parent directory (especially if the file was newly created)?
+
+            File.Delete(sizeFile)
+            state <- EmptyMember { fileState with Size = fileState.Size + int64 compressedMemberSize }
+            lastAppendFinished <- true
+
+    /// Ensures that we don't hold old messages in memory for too long.
+    /// This function is automatically called after appending message.
+    member me.FlushMemberIfTooOld(timestamp : DateTimeOffset) =
+        check ()
+
+        match state with
+        | Initial | EmptyMember _ -> ()
+        | NonEmptyMember (_fileState, decompressedMemberState) ->
+            if timestamp - decompressedMemberState.Timestamp > TimeSpan.FromSeconds config.MaxSecondsBeforeFlush then
+                me.FlushMember()
+
+    member me.AppendMessage(timestamp : DateTimeOffset, message : Span<byte>) =
+        check ()
+
+        if not message.IsEmpty then
+            me.AppendNonEmptyMessage(timestamp, message)
+
+    member private me.AppendNonEmptyMessage(timestamp : DateTimeOffset, message : Span<byte>) =
+        if message.IsEmpty then
+            failwith "Empty message"
+
+        // Ensure we're in correct directory.
+        match state with
+        | Initial -> state <- EmptyMember (createInitialFileState timestamp)
+        | EmptyMember fileState | NonEmptyMember (fileState, _) ->
+            let newDateInPath = timestampToDate timestamp
+
+            // We have to select another dir.
+            if fileState.DateInPath < newDateInPath then
+                me.FlushMember()  // We have to flush BEFORE changing directory.
+
+                // Flush won't change `DateInPath`.
+                // So we don't need to reread `state` after flushing because we're changing `DateInPath`.
+                state <- EmptyMember { DateInPath = newDateInPath
+                                       Name = timestampToFile timestamp
+                                       Size = 0L
+                                     }
+
+        // Ensure that we don't exceed maximum member size.
+        let decompressedMemberState =
+            match state with
+            | Initial -> failwith "Absurd"
+            | EmptyMember _fileState -> { Timestamp = timestamp; Size = 0 }
+            | NonEmptyMember (_fileState, decompressedMemberState) ->
+                if config.MaxDecompressedMemberSize - decompressedMemberState.Size < message.Length then
+                    me.FlushMember()  // Flush preserves `DateInPath` but may change file's `Name` and `Size`.
+                    { Timestamp = timestamp; Size = 0 }
+                else
+                    decompressedMemberState
+
+        let newSize = decompressedMemberState.Size + message.Length
+        // We have to enlarge `decompressedMemberBuffer`.
+        if decompressedMemberBuffer.Memory.Length < newSize then
+            use mutable newDecompressedMemberBuffer = MemoryPool.Shared.Rent(newSize)
+            let old = decompressedMemberBuffer
+            old.Memory.Slice(0, decompressedMemberState.Size).CopyTo(newDecompressedMemberBuffer.Memory)
+            decompressedMemberBuffer <- newDecompressedMemberBuffer
+            newDecompressedMemberBuffer <- old  // This line guarantees that old buffer will be freed.
+
+        message.CopyTo(decompressedMemberBuffer.Memory.Span.Slice(decompressedMemberState.Size))
+
+        // We have to read `fileState` AFTER all calls to `FlushMember`
+        // because these calls may change file's `Name` and `Size`.
+        let fileState =
+            match state with
+            | Initial -> failwith "Absurd"
+            | EmptyMember fileState | NonEmptyMember (fileState, _) -> fileState
+        state <- NonEmptyMember (fileState, { Timestamp = decompressedMemberState.Timestamp; Size = newSize })
+
+        me.FlushMemberIfTooOld(timestamp)
+
+    interface IDisposable with
+        override me.Dispose() =
+            if not disposed then
+                try
+                    // Flush only if everything was alright.
+                    if lastAppendFinished then
+                        me.FlushMember()
+                // TODO Don't catch all exceptions - eg. `OutOfMemoryException`.
+                with e ->
+                    eprintfn "FlushMember during Dispose failed: %A" e
+
+                disposed <- true
+                decompressedMemberBuffer.Dispose()
+                compressedMemberBuffer.Dispose()
+                compressor.Dispose()
